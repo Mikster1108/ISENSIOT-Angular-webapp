0070905027a752d383ceda805c792516eca2cef8
mikal <s1135287@student.hsleiden.nl>
2023-12-17 20:47:51 +0100

    Added error handling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,16 +11,20 @@ const VIDEO_DISPLAY_AMOUNT = 3;
 export class DashboardComponent implements OnInit{
 
   videoUrls: string[] = [];
+  showErrorMessage: boolean = false;
 
   constructor(private videoService: VideoService) { }
 
   loadVideos(): void {
-    this.videoService.getAllVideoLinks('duration', 1).subscribe(
+    this.showErrorMessage = false;
+    this.videoService.getAllVideoLinks('date', 1).subscribe(
         (responseData: any) => {
           let items = responseData.items;
           items = items.splice(0, VIDEO_DISPLAY_AMOUNT);
 
           this.videoUrls = this.videoService.loadVideos(items);
+        }, error => {
+          this.showErrorMessage = true;
         }
     );
   }
