714a55a45637474996b19894b147d69f1de85a4d
mikal <s1135287@student.hsleiden.nl>
2023-12-19 00:39:09 +0100

    Feat: added login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../service/auth.service";
+import {TokenStorageService} from "../service/token.service";
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  email: string = "Email@adress";
+  form: any  = {
+    email: null,
+    password: null
+  }
+
+  email: string | undefined;
   password: string | undefined;
 
-  constructor() { }
+  isLoggedIn: boolean = false;
+  errorStatusCode: number | undefined;
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) {
+  }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+
+      window.location.href="/home";
+    }
+  }
+
+  onSubmit(): void {
+    const {email, password} = this.form;
+
+    this.authService.login(email, password).subscribe(
+        (data: any) => {
+          this.tokenStorage.saveToken(data.token);
+          window.location.href = "/home";
+
+        }, err => {
+          this.isLoggedIn = false;
+            this.errorStatusCode = err.status;
+          }
+    );
+  }
+
+  resetErrorStatusCode() {
+    this.errorStatusCode = undefined;
+    console.log(this.errorStatusCode)
   }
 
 }
