91e7fc37549a1b7a64cf3c27ab26a5236716bef1
mikal <s1135287@student.hsleiden.nl>
2023-12-19 00:39:25 +0100

    Feat: added register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../service/auth.service";
+import {TokenStorageService} from "../service/token.service";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    email: '',
+    password: '',
+    access_code: null
+  }
+
+  isSuccessful: boolean | undefined;
+  errorStatusCode: number | undefined;
+  errorMessage: string | undefined;
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      window.location.href="/home";
+    }
+  }
+
+  onSubmit(): void {
+    const {email, password, access_code} = this.form;
+
+    this.authService.register(email, password, access_code).subscribe(
+        (registerData: any) => {
+          this.isSuccessful = true;
+          this.tokenStorage.saveToken(registerData.token)
+
+          window.location.href="/home";
+        },
+        err => {
+          this.isSuccessful = false;
+          this.errorStatusCode = err.status;
+          this.errorMessage = err.error.error;
+        }
+    );
+  }
+
+  resetErrorVariables(): void {
+    this.isSuccessful = undefined;
+    this.errorStatusCode = undefined;
+    this.errorMessage = undefined;
   }
 
 }
