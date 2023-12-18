23efe9f0a929d668eac2748417736e7a7461f307
mikal <s1135287@student.hsleiden.nl>
2023-12-18 16:43:39 +0100

    Added guard for checking if user is logged in

---
+++ src/app/helpers/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute} from '@angular/router';
+import {TokenStorageService} from "../service/token.service";
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(
+      private router: Router,
+      private activatedRoute: ActivatedRoute,
+      private tokenService: TokenStorageService
+  ) {}
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.tokenService.getToken();
+
+    if (currentUser == null) {
+      void this.router.navigate(['/login']);
+
+      return false;
+    }
+
+    return true;
+  }
+}
