38e218cdc8e8608bddab9530f8a4ce874f347761
mikal <s1135287@student.hsleiden.nl>
2024-01-07 20:02:19 +0100

    WIP: handling camera feed status

--- src/app/live-camera/live-camera.component.ts
+++ src/app/live-camera/live-camera.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SocketService} from "../service/socket.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-live-camera',
@@ -7,55 +8,61 @@ import {SocketService} from "../service/socket.service";
 })
 export class LiveCameraComponent implements OnInit, OnDestroy {
 
-  connected: boolean | undefined;
+  connected: boolean = false
   responseMessage: string | undefined;
   streamStarted: boolean = false;
 
   constructor(private socketService: SocketService) { }
 
   ngOnInit(): void {
-    this.listenForResponse()
+    this.connect().subscribe(() => {}, error => {
+      this.responseMessage = 'Connection to server failed!';
+      this.connected = true;
+    });
   }
 
   ngOnDestroy() {
-    this.socketService.stopStream();
     this.socketService.disconnect();
   }
 
-  connect() {
-    this.socketService.connect().subscribe((response: any) => {
-      if(response.data) {
-        this.connected = true;
-      }
-    }, error => {
-      this.connected = false;
+  connect(): Observable<any> {
+    return this.socketService.connect();
+  }
+
+  disconnect(): void {
+    this.socketService.disconnect();
+    this.streamStarted = false;
+    this.connected = false;
+  }
+
+  startWatchingStream(): void {
+    this.initStream();
+  }
+
+  stopWatchingStream(): void {
+    this.responseMessage = 'Exiting stream...';
+    this.streamStarted = false;
+    this.responseMessage = undefined;
+    this.disconnect();
+  }
+
+  initStream(): void {
+    this.responseMessage = 'Waiting for server response...';
+    this.streamStarted = true;
+    this.socketService.startStream().subscribe((response: any) => {
+          if (response.data) {
+            this.responseMessage = 'Loading stream...'
+          }
+        }, error => {
+          this.streamStarted = false;
+          this.responseMessage = 'Failed to fetch the stream!'
         }
     );
   }
 
-  disconnect() {
-    this.socketService.disconnect();
-    this.connected = false;
+  handleStreamCrash(): void {
+    this.responseMessage = 'Connection to stream was broken!'
     this.streamStarted = false;
-  }
-
-  sendMessage(message: string): void {
-    this.socketService.sendMessage(message);
-  }
-
-  listenForResponse(): void {
-    this.socketService.onMessage().subscribe((response: any) => {
-      this.responseMessage = response.data;
-    });
-  }
-
-  startStream(): void {
-    this.socketService.startStream();
-    this.streamStarted = true;
-  }
-
-  stopStream(): void {
-    this.socketService.stopStream();
   }
 
 }
