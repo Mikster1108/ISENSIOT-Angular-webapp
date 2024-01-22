828b605c6611f1e2973292b18b4b7496c853b8b3
mikal <s1135287@student.hsleiden.nl>
2024-01-22 22:50:20 +0100

    Added table with sensordata

--- src/app/recordings/recording/recording.component.ts
+++ src/app/recordings/recording/recording.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {VideoService} from "../../service/video.service";
 import {ActivatedRoute} from "@angular/router";
+import {TimestampService} from "../../service/timestamp.service";
+import {Sensordata} from "../../model/sensordata";
 
 @Component({
   selector: 'app-recording',
@@ -8,23 +10,59 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class RecordingComponent implements OnInit {
 
-  showErrorMessage: boolean = false;
   videoName: string = '';
   videoUrl: string[] | undefined;
+  video_duration_description: string | undefined;
+  video_analysis_data: Sensordata[] = [];
+  activation_reason: string | undefined;
 
-  constructor(private videoService: VideoService, private route: ActivatedRoute) { }
+  constructor(private videoService: VideoService, private route: ActivatedRoute, private timestampService: TimestampService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.videoName = params['videoName'];
       this.loadVideo();
     });
+
+    this.videoService.getVideoLink(this.videoName).subscribe((response: any) => {
+      let duration_sec = response.duration;
+      const sensor_data = response.analysis_data;
+
+      this.video_duration_description = this.timestampService.convertSecondsToReadableAmount(duration_sec);
+
+      for (let data of sensor_data) {
+        const new_data: Sensordata = new Sensordata(data.timestamp_ms, data.item_found);
+        this.video_analysis_data.push(new_data);
+      }
+
+      const first_item = this.video_analysis_data[0];
+      this.activation_reason = first_item ? first_item.item_found : "Unknown";
+    });
   }
 
   loadVideo(): void {
-    this.showErrorMessage = false;
     let videoName = [];
     videoName.push(this.videoName);
     this.videoUrl = this.videoService.loadVideos(videoName);
   }
+
+  convertVideoNameToTimestamp(filename: string): string {
+    return this.timestampService.convertVideoNameToTimestamp(filename);
+  }
+
+  convertTimestampToDescription(timestamp: string): string {
+    return this.timestampService.convertTimestampToDescription(timestamp);
+  }
+
+  convertNumberTimestampToVideoTimestamp(timestamp: number): string {
+    const seconds = Math.floor(timestamp / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+
 }
